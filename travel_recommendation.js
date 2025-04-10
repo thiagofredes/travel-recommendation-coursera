97a14f8636d961225812da4ca6c8f99c4b61cf0b
HOGARTHWW\thiagofredes <thiago.fredes@hogarth.com>
2025-04-10 10:28:08 -0300

    Initial commit

---
+++ travel_recommendation.js
@@ -0,0 +1,41 @@
+const recommendationsAPI = "/travel_recommendation_api.json";
+
+async function search(searchTerm) {
+    let searchTermLoweCase = searchTerm.toLowerCase();
+    let response;
+
+    switch (searchTermLoweCase) {
+        case "beach":
+        case "beaches":
+            response = await fetchRecommendation("beaches");
+            break;
+
+        case "temple":
+        case "temples":
+            response = await fetchRecommendation("temples");
+            break;
+
+        case "country":
+        case "countries":
+            response = await fetchRecommendation("countries");
+            break;
+    }
+
+    return response;
+}
+
+async function fetchRecommendation(term) {
+    let recommendation;
+    await fetch(recommendationsAPI)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            recommendation = json[term];
+        });
+
+    return recommendation;
+}
